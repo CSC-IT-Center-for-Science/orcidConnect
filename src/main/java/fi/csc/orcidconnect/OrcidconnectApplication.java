c216ed47380697dd384218f014e97d0549a59bf8
Kari Laalo <kari.laalo@csc.fi>
2015-11-27 15:00:18 +0200

    spring init

---
+++ src/main/java/fi/csc/orcidconnect/OrcidconnectApplication.java
@@ -0,0 +1,12 @@
+package fi.csc.orcidconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcidconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrcidconnectApplication.class, args);
+    }
+}
