bdcc9b26d17e8084b27f38759cd26f341ebd8220
karppa <klaalo@csc.fi>
2015-11-28 02:40:45 +0200

    oauth tesstailua

--- src/main/java/fi/csc/orcidconnect/OrcidconnectApplication.java
+++ src/main/java/fi/csc/orcidconnect/OrcidconnectApplication.java
@@ -1,12 +1,33 @@
 package fi.csc.orcidconnect;
 
+
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@RestController
 public class OrcidconnectApplication {
 
-    public static void main(String[] args) {
+	  @RequestMapping("/principal")
+	  public Principal principal(Principal principal) {
+	    return principal;
+	  }
+
+	  @RequestMapping("/auth")
+	  public Authentication auth(Authentication a) {
+	    return a;
+	  }
+
+	  public static void main(String[] args) {
         SpringApplication.run(OrcidconnectApplication.class, args);
-    }
+	  }
+	  
 }
+
