cfdc0509255858cabcbed3ba309b8307f7e14933
karppa <klaalo@csc.fi>
2016-03-05 12:16:33 +0200

    soapclient against weather service

--- src/main/java/fi/csc/orcidconnect/push/soap/SoapClient.java
+++ src/main/java/fi/csc/orcidconnect/push/soap/SoapClient.java
@@ -1,27 +1,38 @@
 package fi.csc.orcidconnect.push.soap;
 
-import java.io.StringReader;
-
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import fi.csc.orcidconnect.push.soap.schema.GetWeatherInformation;
+import fi.csc.orcidconnect.push.soap.schema.GetWeatherInformationResponse;
+import fi.csc.orcidconnect.push.soap.schema.WeatherDescription;
+
 
 public class SoapClient {
 	
-	private static final String MESSAGE =
-	        "<message xmlns=\"http://tempuri.org\">Hello Web Service World</message>";
-
-	private final WebServiceTemplate wsTempl = new WebServiceTemplate();
-	
-	
-	// https://spring.io/guides/gs/consuming-web-service/
     public void customSendAndReceive() {
-        StreamSource source = new StreamSource(new StringReader(MESSAGE));
-        StreamResult result = new StreamResult(System.out);
-        wsTempl.sendSourceAndReceiveToResult("http://wsf.cdyne.com/WeatherWS/Weather.asmx/GetWeatherInformation",
-        //wsTempl.sendSourceAndReceiveToResult("http://klaalo-air13:8088/mockProvisionBinding/receive",
-            source, result);
+    	
+    	WebServiceTemplate wsTempl = new WebServiceTemplate();
+    	Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+    	marshaller.setPackagesToScan("fi.csc.orcidconnect.push.soap.schema");
+
+    	wsTempl.setDefaultUri("http://wsf.cdyne.com/WeatherWS/Weather.asmx");
+    	wsTempl.setMarshaller(marshaller);
+    	wsTempl.setUnmarshaller(marshaller);
+
+    	GetWeatherInformation req = new GetWeatherInformation();
+    	
+    	GetWeatherInformationResponse resp = 
+			(GetWeatherInformationResponse) wsTempl.marshalSendAndReceive(req, 
+			new SoapActionCallback("http://ws.cdyne.com/WeatherWS/GetWeatherInformation"));
+
+    	for (WeatherDescription desc : 
+    		resp.getGetWeatherInformationResult().getWeatherDescription()) {
+    		System.out.println(desc.getWeatherID() + " | " + desc.getDescription());
+     	}
+        
     }
 	
 	
