3ab2a08f336119e68dce43105c683633b310b8c3
karppa <klaalo@csc.fi>
2016-03-09 10:48:08 +0200

    request as json

--- src/main/java/fi/csc/orcidconnect/push/rest/MockRestClient.java
+++ src/main/java/fi/csc/orcidconnect/push/rest/MockRestClient.java
@@ -1,5 +1,8 @@
 package fi.csc.orcidconnect.push.rest;
 
+import java.util.Arrays;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import fi.csc.orcidconnect.IdentitiesRelayer;
@@ -10,7 +13,11 @@ public class MockRestClient extends IdentitiesRelayer {
 
 	@Override
 	public boolean relay(IdentityDescriptor idDescr) {
-		RestTemplate rt = new RestTemplate(); 
+		RestTemplate rt = new RestTemplate();
+		rt.setMessageConverters(
+				Arrays.asList(
+						new MappingJackson2HttpMessageConverter())
+				);
 		Status stat = rt.postForObject(
 				"https://demo9650738.mockable.io/identities",
 				idDescr, Status.class);
