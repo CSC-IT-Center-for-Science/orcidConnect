adaedb8a6d1ea1281a94fbf0cdbee82e78b580ed
Kari Laalo <klaalo@csc.fi>
2016-05-17 09:37:10 +0300

    reduce indentation

--- src/main/java/fi/csc/orcidconnect/WebControllerConfiguration.java
+++ src/main/java/fi/csc/orcidconnect/WebControllerConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix="my.controllerConfig")
+@ConfigurationProperties(prefix="my")
 public class WebControllerConfiguration {
 	
 	
