2b76789b26468db1467a5cb745465136fda33b4f
Kari Laalo <klaalo@csc.fi>
2016-05-27 12:13:36 +0300

    new approach for basic auth

--- src/main/java/fi/csc/orcidconnect/push/rest/RestJsonClient.java
+++ src/main/java/fi/csc/orcidconnect/push/rest/RestJsonClient.java
@@ -1,14 +1,24 @@
 package fi.csc.orcidconnect.push.rest;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Map;
 
+import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.BasicHttpContext;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
@@ -68,9 +78,16 @@ public class RestJsonClient implements IdentitiesRelayer {
 				.setDefaultCredentialsProvider(crProv)
 				.build();
 		
-
+		URL url;
+		try {
+			url = new URL(config.get(restUrl));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
 		HttpComponentsClientHttpRequestFactory reqFac = 
-				new HttpComponentsClientHttpRequestFactory();
+				new LocalRequestFactory(new HttpHost(url.getHost()));
 		reqFac.setHttpClient(httpClient);
 
 		RestTemplate rt = new RestTemplate(reqFac);
@@ -83,6 +100,32 @@ public class RestJsonClient implements IdentitiesRelayer {
 				config.get(restUrl),
 				idDescr, Status.class);
 		return stat.status();
+	}
+	
+	class LocalRequestFactory extends HttpComponentsClientHttpRequestFactory {
+		
+		HttpHost host;
+		
+		public LocalRequestFactory (HttpHost host) {
+			super();
+			this.host = host;
+		}
+		
+		protected BasicHttpContext createHttpContext (HttpMethod httpMethod, URI uri) {
+			return createContext();
+		}
+		
+		private BasicHttpContext createContext() {
+		       AuthCache authCache = new BasicAuthCache();
+		        BasicScheme basicAuth = new BasicScheme();
+		        authCache.put(host, basicAuth);
+		 
+		        // Add AuthCache to the execution context
+		        BasicHttpContext localcontext = new BasicHttpContext();
+		        localcontext.setAttribute(HttpClientContext.AUTH_CACHE, authCache);
+		        return localcontext;			
+		}
+		
 	}
 
 }
